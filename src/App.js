8d8c2a4c9609ea985f2e8bf4b9057563de7a3e7a
buuusy <dev.c.jade@gmail.com>
2022-07-11 17:31:35 +0900

    컴포넌트 사라질때 작업하기

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import { useState, useEffect } from 'react';
 
+function Hello() {
+  function hiFn() {
+    console.log('created');
+    return byeFn;
+  }
+
+  function byeFn() {
+    console.log('bye');
+  }
+
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => {
-    setKeyword(e.target.value);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
   };
-
-  useEffect(() => {
-    console.log('i run only once');
-  }, []);
-
-  useEffect(() => {
-    console.log('i run when "keyword" changes.');
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log('i run when "couter" changes.');
-  }, [counter]);
-
   return (
     <div>
-      <input onChange={onChange} value={keyword} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1> <button onClick={onClick}>click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
