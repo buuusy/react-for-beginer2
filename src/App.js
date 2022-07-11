7a24a65005a50b5d7412e6359be41d61e5ac7d02
buuusy <dev.c.jade@gmail.com>
2022-07-11 16:16:02 +0900

    useEffect 맛보기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Button from './Button';
-import styles from './App.modules.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+
+  const onClick = () => setValue((prev) => prev + 1);
+
+  console.log('i run all the time');
+
+  const iRunOnlyOnce = () => {
+    console.log('i run only once');
+  };
+
+  useEffect(() => {
+    console.log('CALL the api');
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={'continue'} />
+      <h1>{counter}</h1> <button onClick={onClick}>click me!</button>
     </div>
   );
 }
