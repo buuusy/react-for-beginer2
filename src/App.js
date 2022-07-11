e7947fee07f4631ccb6fbb9314d2161b5c577961
buuusy <dev.c.jade@gmail.com>
2022-07-11 16:39:05 +0900

    useEffect 상태값에 따라 업데이트 되게 설정

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
 
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
 
   console.log('i run all the time');
-
-  const iRunOnlyOnce = () => {
-    console.log('i run only once');
-  };
 
   useEffect(() => {
     console.log('CALL the api');
   }, []);
+  // 최초 렌더링만 실행
+
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log('search for', keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input onChange={onChange} value={keyword} type="text" placeholder="Search here..." />
       <h1>{counter}</h1> <button onClick={onClick}>click me!</button>
     </div>
   );
