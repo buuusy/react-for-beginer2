f5c28f967b73ad8ee9800bd3389c37afd143b0a3
buuusy <dev.c.jade@gmail.com>
2022-07-12 10:15:40 +0900

    Todo 리스트만들기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import { useState, useEffect } from 'react';
 
-function Hello() {
-  function hiFn() {
-    console.log('created');
-    return byeFn;
-  }
-
-  function byeFn() {
-    console.log('bye');
-  }
-
-  useEffect(() => {
-    console.log('hi');
-    return () => {
-      console.log('bye');
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
   };
+  console.log(toDos);
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder="Write your to do.." />
+        <button>Add To do</button>
+      </form>
     </div>
   );
 }
