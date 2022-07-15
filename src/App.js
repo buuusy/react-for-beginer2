6f545fc9ff0103cda59c1814505e227c525c65b1
buuusy <dev.c.jade@gmail.com>
2022-07-15 10:40:51 +0900

    영화리스트 만들기 시작. api 받아옴

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
-import React from 'react';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [loading, setLoding] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [Selected, setSelected] = useState('');
-  const regex = /\b[+-]?\d+(?:,\d{3})*(?:\.\d+)?\b/;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  //한번 실행되는 함수
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
+    fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year')
       .then((res) => res.json())
-      .then((json) => {
-        setCoins(json);
-        setLoding(false);
-      });
+      .then((json) => setMovies(json.data.movies));
+    setLoading(false);
   }, []);
 
-  const selectedCoin = (e) => setSelected((prev) => e.target.value);
-  const optionList = coins.filter((coin) => !Selected.includes(coin.name));
-
-  return (
-    <div>
-      <h1>The Coins!({loading ? '' : coins.length})</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={selectedCoin} value={Selected}>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <hr />
-      <h2>Change Coins</h2>
-      <ul>
-        {optionList.map((coin) => (
-          <li key={coin.id}>
-            {coin.name} ({coin.symbol}) :
-            {Selected ? `${coin.quotes.USD.price / Selected.match(regex)} USD` : null}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 };
+
 export default App;
