6cdc2f5f1851232d74a318ee69cf4233d8270a89
buuusy <dev.c.jade@gmail.com>
2022-07-15 11:09:49 +0900

    영화 리스트 완성

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useState, useEffect } from 'react';
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+
   const getMovies = async () => {
-    const response = await fetch(
-      'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year'
-    );
-    const json = await response.json();
+    const json = await (
+      await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year')
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
@@ -16,7 +16,29 @@ const App = () => {
     getMovies();
   }, []);
 
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  console.log();
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt="img" />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
