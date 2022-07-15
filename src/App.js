f713c5351663e63cc9eaff0782d4d7eb1021e4ac
buuusy <dev.c.jade@gmail.com>
2022-07-15 10:45:38 +0900

    api 호출 async 로하기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { useState, useEffect } from 'react';
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year'
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year')
-      .then((res) => res.json())
-      .then((json) => setMovies(json.data.movies));
-    setLoading(false);
+    getMovies();
   }, []);
 
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
